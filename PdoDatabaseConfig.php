ecbe57a19980a856fe01bc8fdb8aff9ae3301c33
David Cavar <wizzard405@gmail.com>
2015-01-30 22:54:57 +0100

    Leftover method removed

--- PdoDatabaseConfig.php
+++ PdoDatabaseConfig.php
@@ -7,8 +7,4 @@ class PdoDatabaseConfig extends \YeahOrm\DatabaseConfig {
     public function getDsn() {
         return 'mysql:host=' . $this->getHost() . ';dbname=' . $this->getDatabase() . ';charset=' . $this->getCharset();
     }
-
-    public function getSchema() {
-        
-    }
 }
